eeeee58a53a39792cc93ea28ced444a2daf593ba
정성훈 <162093700+seonghun0825@users.noreply.github.com>
2024-10-07 23:09:52 +0900

    Update langchaintest.py

--- langchaintest.py
+++ langchaintest.py
@@ -1,9 +1,9 @@
 import json
 from langchain_ollama import OllamaLLM
 
-base_regular = "스케줄을 추가할께 응답은 json의 형식으로 시간이 키값이고, 활동이 value 값으로 응답을 내라 문단을 잘 제작하렴, 빠뜨리는 문법없"
+base_regular = "스케줄을 추가할께 응답은 json의 형식으로 시간이 키값이고, 활동이 value 값으로 응답을 내라 문단을 잘 제작하렴, 빠뜨리는 문법없이"
 model = OllamaLLM(model="example")
-response = model.invoke(base_regular+" 10시 기상 12시 출근 5시 퇴")
+response = model.invoke(base_regular+" 10시 기상 12시 출근 5시 퇴근")
 
 # 응답을 JSON 형식으로 변환
 response_json = json.dumps({"response": response}, ensure_ascii=False, indent=4)
@@ -11,7 +11,7 @@ print(response_json)
 
 data = {
     "question": "스케줄입력정보",
-    "context": "8시 기상, 10시 퇴"
+    "context": "8시 기상, 10시 퇴근"
 }
 
 # JSON 데이터를 문자열로 변환
